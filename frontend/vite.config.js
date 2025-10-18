74edafc62200e0a91c4513b104885eb396e23a32
IT2357 <it23571020@my.sliit.lk>
2025-10-18 18:37:56 +0530

    fix(frontend): update api proxy target port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:5002", // 👈 Your backend server
+        target: "http://localhost:5000", // 👈 Your backend server
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
